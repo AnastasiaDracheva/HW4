5c48e43cb1d0190e5e4e5a33717fb67e12df0c83
AnastasiaDracheva <157720677+AnastasiaDracheva@users.noreply.github.com>
2024-02-09 00:08:06 +0200

    Work No. 4 is ready

--- src/Figure.java
+++ src/Figure.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Figure {
+public class Figure {
+   int height;
+   int widht;
+   int lenght;
+
+   int a;
+   int b;
+   int h;
+
+
+
+
+   public void myTriangles(){
+
+   }
+
+   public void myTriangleOptions(String massage){
+
+      System.out.println(massage);
+
+   }
+
+   public double calculateVolume(){
+
+      lenght = 12;
+      widht = 16;
+      height = 7;
+
+      return (double) this.lenght * this.widht * this.height;
+   }
+
+   public int calculateSquare(){
+
+      a = 2;
+      b = 8;
+      h = 4;
+
+      return (int) 2*(this.a * this.b) + (this.a * this.h) + (this.b * this.h);
+
+   }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Figure myTriangleOne = new Figure();
+        Figure myTriangleTwo = new Figure();
+        Figure myTriangleThree = new Figure();
+        Figure myTriangleFour = new Figure();
+
+        myTriangleTwo.height = 12;
+        myTriangleTwo.widht = 6;
+        myTriangleTwo.lenght = 6;
+
+
+
+        System.out.println(myTriangleOne);
+
+        System.out.println("My triangle two height = " + myTriangleTwo.height );
+        System.out.println("My triangle two lenght = " + myTriangleTwo.lenght );
+        System.out.println("My triangle two widht = " + myTriangleTwo.widht );
+
+        System.out.println("Triangle three V = " + myTriangleThree.calculateVolume ());
+
+        System.out.print("Triangle four S =  " + myTriangleFour.calculateSquare());
+
+
     }
+
+
 }
